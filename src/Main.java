3a9ef3d71f86799b138884105aee346ee5dc3fc2
Valery Golovin <v.s.g.clev@gmail.com>
2021-04-11 23:08:08 +0300

    Задание 2

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class Main {
+
+public static boolean matches(String text, String regex) {
+    try {
+        return Pattern.matches(regex, text);
+    } catch (PatternSyntaxException e) {
+        return false;
+    }
+}
+
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String s1 = input.nextLine();
+    String s2 = input.nextLine();
+
+    System.out.println(matches(s1, s2));
+}
+
+}
